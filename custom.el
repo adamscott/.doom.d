b5f4b30b6379c0613f945c6a43d828e7ab2b616a
Adam Scott <ascott.ca@gmail.com>
2025-03-12 10:16:14 -0400

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((projectile-project-compilation-cmd . "make html -j4")
+   '((projectile-project-compilation-cmd . "npm install && npm run build")
+     (setq +format-with :none)
+     (projectile-project-compilation-cmd . "make html -j4")
      (projectile-project-compilation-cmd . "make html")
      (projectile-project-compilation-cmd . "scons"))))
 (custom-set-faces
