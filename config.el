9f7f191673a9c23afc443567f0885ab80e3195b9
Adam Scott <ascott.ca@gmail.com>
2025-05-12 09:34:35 -0400

    Bind javascript to typescript-mode

--- config.el
+++ config.el
@@ -100,5 +100,12 @@
       :desc "Activates the signature"
       "c SPC" #'lsp-signature-activate)
 
+(add-to-list 'auto-mode-alist '("\\.js\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.cjs\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.mjs\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.cts\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.mts\\'" . typescript-mode))
+
 ;; Local config (not in repo)
 (load! "+local.el")
