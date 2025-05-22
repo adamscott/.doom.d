6a5b3409a9a4a2c3904e9146845b26b44050ce90
Adam Scott <ascott.ca@gmail.com>
2025-05-22 00:18:37 -0400

    Add Deno support

--- config.el
+++ config.el
@@ -107,17 +107,38 @@
 (add-to-list 'auto-mode-alist '("\\.cts\\'" . typescript-mode))
 (add-to-list 'auto-mode-alist '("\\.mts\\'" . typescript-mode))
 
-;; (use-package! apheleia
-;;   :init
-;;   (use-package! lsp-biome
-;;     :init
-;;     (setq lsp-biome-format-on-save t
-;;           lsp-biome-active-file-types (list (rx "." (or "tsx" "jsx"
-;;                                                         "ts" "js"
-;;                                                         "mts" "mjs"
-;;                                                         "cts" "cjs"
-;;                                                         "json" "jsonc")
-;;                                                 eos)))))
+(after! lsp-mode
+  ;; Add missing deno.enablePaths
+  (defcustom asc-lsp-clients-deno-enable-paths nil
+    "Controls if the Deno Language Server is enabled for only specific paths of the workspace folder."
+    :group 'lsp-deno
+    :risky t
+    :type '(repeat string))
+  (defcustom asc-lsp-clients-deno-disable-paths nil
+    "Controls if the Deno Language Server is disabled for only specific paths of the workspace folder."
+    :group 'lsp-deno
+    :risky t
+    :type '(repeat string))
+  (defun asc-lsp-clients-deno--make-init-options-advice (original-func &rest args)
+    "Add missing parameter."
+    (let ((new-options (plist-put (apply original-func args) :enablePaths asc-lsp-clients-deno-enable-paths)))
+      new-options))
+  (advice-add 'lsp-clients-deno--make-init-options :around #'asc-lsp-clients-deno--make-init-options-advice)
+
+  ;; Ignore some notifications.
+  (defcustom asc-lsp-ignore-notification-rules '("deno/didRefreshDenoConfigurationTree" "deno/didChangeDenoConfiguration" "deno/didUpgradeCheck")
+    "Notifications to ignore."
+    :group 'lsp-mode
+    :risky t
+    :type '(repeat string))
+  (defun asc-lsp-ignore-notifications-advice (_workspace notification)
+    "Ignore deno notification"
+    (when (member (plist-get notification :method) asc-lsp-ignore-notification-rules)
+      (progn
+        (lsp--info (concat "Ignored '" (plist-get notification :method) "' notification"))
+        ;; Return t to indicate that the notification is handled.
+        t)))
+  (advice-add 'lsp--on-notification :before-until #'asc-lsp-ignore-notifications-advice))
 
 ;; Local config (not in repo)
 (load! "+local.el")
--- custom.el
+++ custom.el
@@ -1,15 +1,19 @@
+;;; custom.el -*- lexical-binding: t; -*-
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '(
-     (projectile-project-compilation-cmd . "npm install && npm run build")
-     (setq +format-with :none)
-     (projectile-project-compilation-cmd . "make html -j4")
-     (projectile-project-compilation-cmd . "make html")
-     (projectile-project-compilation-cmd . "scons"))))
+    '((eval progn
+        (setq lsp-disabled-clients
+          '((rjsx-mode . ts-ls) (typescript-mode . ts-ls)))
+        (setq asc-lsp-clients-deno-enable-paths '("./platform/web" "./misc")))
+       (eval progn
+         (setq lsp-disabled-clients
+           '((rjsx-mode . ts-ls) (typescript-mode . ts-ls))))
+       (projectile-project-compilation-cmd . "scons"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
