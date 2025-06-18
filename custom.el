b595130de7c44376dcdd43d53259cdd13e92cf33
Adam Scott <ascott.ca@gmail.com>
2025-06-18 11:10:51 -0400

    Update custom.el

--- custom.el
+++ custom.el
@@ -6,23 +6,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-    '((eval progn
-        (setq lsp-disabled-clients
-          '((rjsx-mode . ts-ls) (typescript-mode . ts-ls))))
-       (apheleia-formatter denofmt-jsonc) (apheleia-formatter denofmt-json)
-       (apheleia-formatter denofmt-js)
-       (eval progn
-         (setq lsp-disabled-clients
-           '((rjsx-mode . ts-ls) (typescript-mode . ts-ls)))
-         (defun asc-apheleia-formatter-json-hook nil
-           (let*
-             ((file-name (buffer-file-name))
-               (extension (when file-name (file-name-extension file-name))))
-             (if (equal extension "jsonc")
-               (setq apheleia-formatter '(denofmt-jsonc))
-               (setq apheleia-formatter '(denofmt-json)))))
-         (add-hook 'json-ts-mode 'asc-apheleia-formatter-json-hook))
-       (projectile-project-compilation-cmd . "scons"))))
+   '((prettier-mode . 0) (apheleia-formatter denofmt-ts)
+     (apheleia-formatter quote (denofmt-ts))
+     (eval progn
+      (setq lsp-disabled-clients
+            '((rjsx-mode . ts-ls) (typescript-mode . ts-ls))))
+     (apheleia-formatter denofmt-jsonc) (apheleia-formatter denofmt-json)
+     (apheleia-formatter denofmt-js)
+     (eval progn
+      (setq lsp-disabled-clients
+            '((rjsx-mode . ts-ls) (typescript-mode . ts-ls)))
+      (defun asc-apheleia-formatter-json-hook nil
+        (let*
+            ((file-name (buffer-file-name))
+             (extension (when file-name (file-name-extension file-name))))
+          (if (equal extension "jsonc")
+              (setq apheleia-formatter '(denofmt-jsonc))
+            (setq apheleia-formatter '(denofmt-json)))))
+      (add-hook 'json-ts-mode 'asc-apheleia-formatter-json-hook))
+     (projectile-project-compilation-cmd . "scons"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
