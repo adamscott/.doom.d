0ca4b76dd5520feeb2435e09118ea481fcc0e964
Adam Scott <ascott.ca@gmail.com>
2025-11-20 12:24:10 -0500

    Add WIP of flake

---
+++ flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable } @ inputs: {
+
+  };
+}
